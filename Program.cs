005362b5032b2fd78dc2034b049338488988aa48
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-24 01:11:58 +0530

    UseCase 1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
-﻿namespace Day_8_Adress_Book_Assignment
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Day_8_Adress_Book_Assignment
 {
    public class Program
-    {
+   {
+        public string firstname, lastname, mobilenumber, address, city, state, zip, email;
         public static void Main(string[] args)
         {
-
             Console.WriteLine("Hello, Welcome to the Adress Book Program: ");
+
+            
         }
-    }
+   }
 }
