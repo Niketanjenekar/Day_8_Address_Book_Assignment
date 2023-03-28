814a77a1114c3714afc56a9e225e6e765d760e34
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-28 13:24:01 +0530

    Use case 3 to edit the existing contact

--- Program.cs
+++ Program.cs
@@ -7,9 +7,15 @@ namespace Day_8_Adress_Book_Assignment
         public string firstname, lastname, mobilenumber, address, city, state, zip, email;
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to the Adress Book Program: ");
-            ContactManagement_UC1.CreateContact();
-            
+            ContactManagement_UC3 obj = new ContactManagement_UC3();
+            obj.Usecase3_ContactManagement();
+
+        //    //Console.WriteLine("Hello, Welcome to the Adress Book Program: ");
+        //    //ContactManagement_UC1.CreateContact();
+
+        //    ContactManagement_UC3 num = new ContactManagement_UC3();
+        //    num.EditContact();
+
         }
    }
 }
